81c5deed22ca2f18341814bd9313fac23a4c0a96
Thiagomatheus12 <thi_ago5@hotmail.com>
2022-11-30 17:11:31 -0300

    :sparkles: feat - finalização do projeto implementando funcionalidade nos inputs e botões

--- app-to-do/src/app/modules/home/components/todo-input-add-itens/todo-input-add-itens.component.ts
+++ app-to-do/src/app/modules/home/components/todo-input-add-itens/todo-input-add-itens.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-todo-input-add-itens',
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-input-add-itens.component.scss']
 })
 export class TodoInputAddItensComponent implements OnInit {
+  //Evento que exporta o valor vindo do input.
+  @Output() public emitItemTaskList = new EventEmitter();
+  public addItemTaskList: string = "";
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  //Método que envia os dados preenchido no campo de input para adicionar itens na lista.
+  public submitItemTaskList() {
+    this.addItemTaskList = this.addItemTaskList.trim(); // remove todos os espaços, não podendo adicionar string com espaços vazios. no inicio e no fim do input.
+    if(this.addItemTaskList) { //se o input não estiver preenchido não adicionar a lista.
+      this.emitItemTaskList.emit(this.addItemTaskList);
+      this.addItemTaskList = "";
+    }
+  }
 }
--- app-to-do/src/app/modules/home/components/todo-list/todo-list.component.ts
+++ app-to-do/src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,15 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck } from '@angular/core';
+
+// Interface
+import { TaskList } from './../../model/task-list';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
-export class TodoListComponent implements OnInit {
+export class TodoListComponent implements DoCheck {
+
+  public taskList: Array<TaskList> = JSON.parse(localStorage.getItem("list") || '[]');
 
   constructor() { }
 
-  ngOnInit(): void {
+  //Todos os itens marcados como checked vão para o fim da lista.
+  ngDoCheck() {
+    this.setLocalStorage();
+   }
+
+  //Método que recupera o valor recebido do @Output do componente todo-input-add-itens. E adiciona ele a lista.
+  public setEmitTaskList(event: string) {
+    this.taskList.push({task: event, checked: false});
   }
 
+  //Método que deleta um item de cada vez da lista atraves do seu posicionamento no Array.
+  public deleteItemTaskList(event: number) {
+    this.taskList.splice(event, 1); //remove o item da lista a partir do seu index no Array
+  }
+
+  //Método que Deleta todos os itens dentro da lista.
+  public deleteAllTaskList() {
+    const confirm = window.confirm("Você deseja realmente Deletar tudo?"); //um alert para confirmação de remoção de todos os item da lista.
+
+    if(confirm) { //se o usuário confirmar sera removido todos os itens da lista.
+      this.taskList = [];
+    }
+  }
+
+  //Método que verifica se o input está preenchido, caso não esteja ele pergunta se quer deletar.
+  public validationInput(event: string, index: number) {
+    if(!event.length) {
+      const confirm = window.confirm("Task está vazia, deseja deletar?");
+      if(confirm) {
+        this.deleteItemTaskList(index);
+      }
+    }
+  }
+
+  //Método que salva os dados preenchidos no input para o browser.
+  public setLocalStorage() {
+    if(this.taskList) {
+      this.taskList.sort( (first, last) => Number(first.checked) - Number(last.checked));
+      localStorage.setItem("list", JSON.stringify(this.taskList)); //Converte os dados do Array para uma string.
+    }
+  }
 }
